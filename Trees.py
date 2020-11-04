fd88115c2f23eb292b6f7d3edff7112460882337
VincentBlacksea <vincent.bulut@gmail.com>
2020-11-05 00:27:35 +0100

    Initial Comment

---
+++ Trees.py
@@ -0,0 +1,18 @@
+
+import os
+#chemin = "/mnt/c/Users/vince/OneDrive/Bureau"
+#LastMinuteChange
+chemin = "C:\\Users\\vince\\OneDrive\\Bureau"
+
+def afficherFic(lstRep, largeur):
+	for unFic in lstRep:
+		print("/".rjust(largeur+1) + unFic)
+
+def afficherRep(entreeRep):
+	print(entreeRep[0] + "/")
+	afficherFic(entreeRep[2], len(entreeRep[0]))
+
+arbre = os.walk(chemin)
+for unRep in arbre:
+	afficherRep(unRep)
+
